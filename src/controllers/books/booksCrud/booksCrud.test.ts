59724d881186900eeeca6f39b2aa83baaba87ce2
Ayose Suárez Turrumblú (CS) <ayose.suarezturrumbl@gendigital.com>
2024-04-02 11:36:58 +0100

    * Added some tests for delete book

--- src/controllers/books/booksCrud/booksCrud.test.ts
+++ src/controllers/books/booksCrud/booksCrud.test.ts
@@ -2,15 +2,17 @@ import app from "../../../index";
 import request, {Response} from "supertest";
 import Book from "../../../models/book";
 
-const generateRandomISBN = (): string => {
-    let isbn = '978';
+const validAuthorId = '660bcd300ccc4c51cfad0850',
+    badAuthorId = 'bad_author_id',
+    generateRandomISBN = (): string => {
+        let isbn = '978';
 
-    for (let i = 0; i < 10; i++) {
-        isbn += Math.floor(Math.random() * 10);
-    }
+        for (let i = 0; i < 10; i++) {
+            isbn += Math.floor(Math.random() * 10);
+        }
 
-    return isbn;
-};
+        return isbn;
+    };
 
 describe('Books CRUD Controller', () => {
     let createdBookIds: string[] = [];
@@ -22,9 +24,6 @@ describe('Books CRUD Controller', () => {
     });
 
     describe('POST /books', () => {
-        const validAuthorId = '660bcd300ccc4c51cfad0850',
-            badAuthorId = 'bad_author_id';
-
         it('should return 400 if no authors provided', async () => {
             const response: Response = await request(app)
                 .post('/books')
@@ -81,6 +80,37 @@ describe('Books CRUD Controller', () => {
                 });
 
             expect(response.status).toBe(400);
+        });
+    });
+
+    describe('DELETE /books/:id', () => {
+        it('should return 200 if book is successfully deleted', async () => {
+            const createResponse: Response = await request(app)
+                .post('/books')
+                .send({
+                    title: 'Test Book to Delete',
+                    authors: [validAuthorId],
+                    _id: generateRandomISBN(),
+                    price: 20.5
+                });
+
+            const bookIdToDelete = createResponse.body._id;
+
+            createdBookIds.push(bookIdToDelete);
+
+            const deleteResponse: Response = await request(app)
+                .delete(`/books/${bookIdToDelete}`);
+
+            expect(deleteResponse.status).toBe(200);
+        });
+
+        it('should return 404 if book to delete is not found', async () => {
+            const invalidBookId = 'invalid_book_id';
+
+            const response: Response = await request(app)
+                .delete(`/books/${invalidBookId}`);
+
+            expect(response.status).toBe(404);
         });
     });
 });
